3ad923f447e1d536a0c90f611ac11e405b4cc9a3
rcasalla <rcasalla@uniandes.edu.co>
2016-09-05 18:29:25 -0500

    Relacion Book reviews

--- src/main/webapp/src/modules/books/books.mod.js
+++ src/main/webapp/src/modules/books/books.mod.js
@@ -2,47 +2,55 @@
     var mod = ng.module("bookModule", ["ngMessages"]);
 
     mod.constant("booksContext", "api/books");
- 
+
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
             var basePath = 'src/modules/books/';
             $urlRouterProvider.otherwise("/booksList");
-     
-            $stateProvider.state('booksList', {
+
+            $stateProvider.state('books', {
                 url: '/books',
+                abstract: true,
                 views: {
                     'mainView': {
+                        controller: 'booksCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'books.html'
+                    }
+                }
+            }).state('booksList', {
+                url: '/list',
+                parent: 'books',
+                views: {
+                    'bookView': {
                         controller: 'booksCtrl',
                         controllerAs: 'ctrl',
                         templateUrl: basePath + 'books.list.html'
                     }
                 }
             }).state('bookCreate', {
-                url: '/books/create',
+                url: '/create',
+                parent: 'books',
                 views: {
-                    'mainView' : {
+                    'bookView': {
                         controller: 'booksCtrl',
                         controllerAs: 'ctrl',
                         templateUrl: basePath + 'books.create.html'
                     }
                 }
-
             }).state('bookEdit', {
-                url: '/books/:bookId',
-                param: {
-                    bookId: null
-                },
+                url: '/{bookId:int}/edit',
+                param: { 'bookId' : null},
+                parent: 'books',
                 views: {
-                    'mainView': {
+                    'bookView': {
                         controller: 'booksCtrl',
                         controllerAs: 'ctrl',
                         templateUrl: basePath + 'books.create.html'
                     },
-                    'bottomView': {
-                        controller: 'reviewsCtrl',
-                        controllerAs: 'ctrl',
-                        templateUrl: basePath + 'books.reviews.html'
+                    'childsView': {
+                        templateUrl: basePath + 'books.instance.html'
                     }
-                    }               
+                }
             });
         }]);
 })(window.angular);
--- src/main/webapp/src/modules/reviews/reviews.mod.js
+++ src/main/webapp/src/modules/reviews/reviews.mod.js
@@ -1,7 +1,33 @@
 (function (ng) {
     
-    var mod = angular.module('reviewModule', ["ngMessages"]);
-    
-    mod.constant("reviewContext", "/reviews");
-    
+    var mod = angular.module('reviewModule',[]
+    );
+    mod.constant("reviewsContext", "/reviews");
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            var basePath = 'src/modules/reviews/';
+            $urlRouterProvider.otherwise("/reviewsList");
+     
+            $stateProvider.state('reviewsList', {
+                url: '/reviews',
+                parent: 'bookEdit',
+                views: {
+                    'bookInstanceView': {
+                        controller: 'reviewsCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'reviews.list.html'
+                    }
+                }
+            }).state('reviewCreate', {
+                url: '/reviews/create',
+                parent: 'bookEdit',
+                views: {
+                    'bookInstanceView': {
+                        controller: 'reviewsCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'reviews.create.html'
+                    }
+                }
+
+            });
+        }]);
 })(window.angular);
